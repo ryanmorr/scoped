6ccce086a9e729fb857f67bcb8357a61115b70ff
Ryan Morr <rm.morr@gmail.com>
2020-05-22 13:00:35 -0400

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import minify from 'rollup-plugin-babel-minify';
+
+const banner = `/*! ${pkg.name} v${pkg.version} | ${pkg.homepage} */`;
+
+export default {
+    input: 'src/scoped.js',
+    output: [
+        {
+            banner,
+            name: 'scoped',
+            file: pkg.browser,
+            format: 'umd'
+        },
+        {
+            banner,
+            file: pkg.main,
+            format: 'cjs'
+        },
+        {
+            banner,
+            file: pkg.module,
+            format: 'esm'
+        }
+    ],
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        commonjs(),
+        minify()
+    ]
+};
